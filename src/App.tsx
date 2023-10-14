5cf37414438965f66e42a15e18b2c1fdab4712bc
Jacob Ebey <jacob.ebey@live.com>
2023-10-13 21:16:36 -0700

    add react router support

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,56 @@
 import React from 'react';
 import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
+import { Link } from "@remix-run/react"
 
-const HeroWithCTAs: React.FC = () => {
-    return (
-        <section className="text-center py-20 bg-gray-100 dark:bg-black">
-            <h1 className="text-4xl mb-4 font-bold text-gray-800 dark:text-white">
-                Go quickly from idea to prototype.
-            </h1>
-            <p className="text-xl mb-10 text-gray-500 dark:text-gray-300">
-                Generate new, or iterate on existing React components.
-            </p>
-            <div className="flex justify-center gap-4">
-                <Button variant="outline" name="signInBtn">
-                    Sign in
-                </Button>
-                <Button variant="ghost" name="signUpBtn">
-                    Sign up
-                </Button>
+const LoginWithSignup = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 dark:bg-black py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <div>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900 dark:text-white">
+            Sign in to your account
+          </h2>
+          <p className="mt-2 text-center text-sm text-gray-600 dark:text-white">
+            Or <Link to="/signup" className="font-medium text-indigo-600 hover:text-indigo-500">
+              start your 30-day free trial
+            </Link>
+          </p>
+        </div>
+        <form className="mt-8 space-y-6" action="#" method="POST">
+          <input type="hidden" name="remember" value="true" />
+          <div className="rounded-md shadow-sm -space-y-px">
+            <div>
+              <Input
+                name="email"
+                type="email"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Email address"
+              />
             </div>
-        </section>
-    )
-}
+            <div>
+              <Input
+                name="password"
+                type="password"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Password"
+              />
+            </div>
+          </div>
+          <div>
+            <Button
+              type="submit"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Sign in
+            </Button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default HeroWithCTAs;
+export default LoginWithSignup;
